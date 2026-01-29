50bc98510e2071951262b356e880b064004e698e
aloky06 <104660778+aloky06@users.noreply.github.com>
2026-01-29 12:51:38 +0530

    bbaba

--- src/components/about/TeamMember.jsx
+++ src/components/about/TeamMember.jsx
@@ -13,13 +13,13 @@ const teamMember = [
     description: "https://www.linkedin.com/in/ceoalokkumaryadav",
   },
   {
-    image: "/team/kaal_bhairav_kashi2.png",
+    image: "/team/Kaal_bhairav_kashi2.png",
     name: "Shree Kaal Bhairav",
     Designation: "Partner",
     description: "https://www.linkedin.com/in/",
   },
   {
-    image: "/team/khatu Shyam.jpg",
+    image: "/team/KhatuShyam.jpg",
     name: "Shree Khatu Shyam ji",
     Designation: "Partner",
     description: "https://www.linkedin.com/in/",
